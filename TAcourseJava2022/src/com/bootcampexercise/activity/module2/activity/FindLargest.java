caa469ed685b4f28623838a8bdd8697faae8db9a
JanisKarklins <45259743+JanisKarklins@users.noreply.github.com>
2022-07-25 16:58:17 +0300

    Find largest

---
+++ TAcourseJava2022/src/com/bootcampexercise/activity/module2/activity/FindLargest.java
@@ -0,0 +1,15 @@
+package com.bootcampexercise.activity.module2.activity;
+
+public class FindLargest {
+    public static void main(String[] args) { //activity 3 Find the largest int
+
+        int a = 50;
+        int b = 150;
+
+        if(a>b)
+            System.out.println(a +" is greateer than "+b);
+        else
+            System.out.println(b +" is greateer than "+a);
+
+        }
+}
