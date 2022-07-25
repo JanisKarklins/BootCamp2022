3987cff82ce76976c04064123bb7fcd41efee1c8
JanisKarklins <45259743+JanisKarklins@users.noreply.github.com>
2022-07-25 15:07:30 +0300

    int 2 push

---
+++ TAcourseJava2022/src/VariableAssignmentActivity/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,9 @@
+package VariableAssignmentActivity.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+        int A =10;
+        A =20+20;
+        System.out.println(A);
+    }
+}
