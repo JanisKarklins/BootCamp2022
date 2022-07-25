bdb6bf92a40a534d384c3487ccd371f640442724
JanisKarklins <45259743+JanisKarklins@users.noreply.github.com>
2022-07-25 16:32:45 +0300

    atcivity 2

---
+++ TAcourseJava2022/src/com/bootcampexercise/activity/module2/activity/OperatorActivity.java
@@ -0,0 +1,14 @@
+package com.bootcampexercise.activity.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) { //activity 2 Find the sum of the given integers
+
+        int a = 50;
+        int b = 150;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+
+}
