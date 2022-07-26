77c8d1c74ebabf7490e7b6cc140a059d6bb75e0d
JanisKarklins <45259743+JanisKarklins@users.noreply.github.com>
2022-07-26 10:03:46 +0300

    PrintNumWithWhileOdd

---
+++ TAcourseJava2022/src/com/bootcampexercise/activity/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,15 @@
+package com.bootcampexercise.activity.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+
+        // Print all even numbers less than 100
+        int i = 1;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 2;
+        }
+
+        //TODO: write code to Print all odd numbers less than 100
+    }
+}
