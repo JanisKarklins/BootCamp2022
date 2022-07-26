cda34bece2dc56695dd2aa23aaede40d45a018c3
JanisKarklins <45259743+JanisKarklins@users.noreply.github.com>
2022-07-26 09:28:21 +0300

    Addwholenumber
    removed out folder

---
+++ TAcourseJava2022/src/com/bootcampexercise/activity/module2/activity/AddWholeNum.java
@@ -0,0 +1,12 @@
+package com.bootcampexercise.activity.module2.activity;
+
+public class AddWholeNum {
+    public static void main(String[] args) { //activity 4 AddWholeNum
+
+        int sum = 0;
+        for (int i = 50; i <= 100; i++) {
+            sum = sum +i;
+        }
+        System.out.println("sum of first 50 numbers is" + sum);
+    }
+}
