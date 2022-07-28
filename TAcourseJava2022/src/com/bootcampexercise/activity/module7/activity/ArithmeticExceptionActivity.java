dc0deea469480602de0d9642693f455e3b80db16
JanisKarklins <45259743+JanisKarklins@users.noreply.github.com>
2022-07-28 12:45:49 +0300

    Module7Activity3

--- TAcourseJava2022/src/com/bootcampexercise/activity/module7/activity/ArithmeticExceptionActivity.java
+++ TAcourseJava2022/src/com/bootcampexercise/activity/module7/activity/ArithmeticExceptionActivity.java
@@ -3,19 +3,34 @@ package com.bootcampexercise.activity.module7.activity;
 // Needs to be completed
 public class ArithmeticExceptionActivity {
     //TODO: 1 - Type main method
-
+    public static void main(String[] args) {
     //TODO: 2  Create an instance of ArithmeticExceptionActivity and call catchMe with 10 and 0
 
+        ArithmeticExceptionActivity arithmeticExceptionActivity = new ArithmeticExceptionActivity();
+        arithmeticExceptionActivity.catchMe(10, 0);
+        arithmeticExceptionActivity.catchMe(10, 2);
+        System.out.println(arithmeticExceptionActivity);
+    }
     void catchMe(int num1, int num2) {
         //The following code results in an Exception.
         //TODO: 3 - Identify the exception and write code to handle this exception.
+
+        try {
         int result = num1 / num2;
         System.out.println("The result is :" + result);
-
+        }catch(ArithmeticException arithmeticException) {
+            System.out.println("Division by 0 is not possible");
+        }
         //TODO: 4 - After handling the exception, write a finally block which
-        //prints a message "Thank you for using this program."
 
+
+        //prints a message "Thank you for using this program."
+        finally{System.out.println("Thank you for using this program.");
+        }
         //TODO: 5- try to call catchMe with 10 and 2 now and see which messages get printed.
+
+        int result = num1 / num2;
+        System.out.println("The result is :" + result);
 
     }
 
--- TAcourseJava2022/src/com/bootcampexercise/activity/module7/activity/sample/ExceptionDeclarationSample.java
+++ TAcourseJava2022/src/com/bootcampexercise/activity/module7/activity/sample/ExceptionDeclarationSample.java
@@ -8,9 +8,9 @@ public class ExceptionDeclarationSample {
             //1 - Throw an instance of  IllegalArgumentException with a String parameter
             //The parameter passed would be printed in the output
             throw new IllegalArgumentException("parameter age cannot be less than 0");
-
-
         }
+    System.out.println("If block finished");
+
     }
 
     public static void main(String arg[]) {
